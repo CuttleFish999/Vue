906693d630d5a867424b4614bc694773429501e2
CuttleFish <b9804320975@gmail.com>
2024-03-22 17:59:48 +0800

    ref

--- src/components/Preson.vue
+++ src/components/Preson.vue
@@ -9,38 +9,29 @@
     </div>
 </template>
 
-<script lang="ts">
-export default {
-    name: 'Person',
-    setup(){
 
-        //  Data
-
-        let name = '文生狗'
-        let age = 18
-        let tel = '1388888'
-
-        // function
-
-        function changeName(){
-            name = "ahzng-san"
+<script setup lang="ts" name="Person324">
+    import {ref} from 'vue'
+    let name = ref('文生狗')
+    let age = ref(18)
+    let tel = '1388888'
+    function changeName(){
+            if(name.value === "文生狗"){
+                name.value = "Dog"
+            }else{
+                name.value = "文生狗"
+            }
+            
         }
 
         function changeAge(){
-            age ++
+            age.value ++
         }
-
-        function showTel(){
+    function showTel(){
             alert(tel)
         }
-
-
-        return {name , age , tel , changeName , changeAge , showTel}  
-        
-    }
-    
-}
 </script>
+
 
 <style scoped>
 .person {
