5707e77198dc560d3e84632e9463d8fdb8e80776
CuttleFish <b9804320975@gmail.com>
2024-03-22 20:40:17 +0800

    computed

--- src/components/Preson.vue
+++ src/components/Preson.vue
@@ -1,66 +1,40 @@
 <template>
     <div class="person">
-        <h2>姓名:{{ dog.name }}，年齡:{{ dog.age }}</h2>
-        
-        <button @click="changeName">修改名子</button>
+        <label for="">姓:<input type="text" v-model="firstName"></label>
+        <label for="">名:<input type="text" v-model="lastName"></label>
+        <h1>全名:{{ fullName }}</h1>
         <button @click="changeAge">修改年齡</button>
-        <button @click="showInfo">显示信息</button>
     </div>
-
-    <div>
-        <h2>遊戲列表</h2>
-        <ul>
-            <li v-for="g in games" v-bind:key="g.id">{{g.name}}</li>
-        </ul>
-        <button @click="changeFirstGame">修改遊戲名First</button>
-    </div>
-    <hr>
-    <h2>測試:{{obj.a.b.c}}</h2>
-    <button @click="changeOBJ">測試</button>
 </template>
 
 
+
 <script setup lang="ts" name="Person324">
-    import {reactive} from 'vue'
-    let dog = reactive({name:"文生狗" , age:18})
-    let games = reactive([
-        {id: "01" , name: "喵"},
-        {id: "02" , name: "喵2"},
-        {id: "03" , name: "喵3"}
-    ])
+import { ref, computed } from 'vue'
 
-    let obj = reactive({
-        a:{
-            b:{
-                c:666
-            }
-        }
-    })
+let firstName = ref('娘')
+let lastName = ref('文生')
 
-    function changeName(){
-            if(dog.name === "文生狗"){
-                dog.name = "Dog"
-            }else{
-                dog.name = "文生狗"
-            }
-            
-        }
+let fullName = computed(() => {
+    return firstName.value.slice(0, 1).toUpperCase() + firstName.value.slice(1) + '-' + lastName.value
+})
 
-    function changeAge(){
-        dog.age++
-    }
+function changeName() {
+    name.value += '~'
+}
 
-    function changeFirstGame(){
-        games[0].name = "貓貓貓"
-    }
-
-    function changeOBJ(){
-        obj.a.b.c = 999
-    }
+function changeAge() {
+    age.value++
+}
 </script>
 
 
 <style scoped>
+input,
+label {
+    font-size: 2em;
+}
+
 .person {
     background-color: skyblue;
     box-shadow: 0 0 10px;
@@ -68,11 +42,11 @@
     padding: 20px;
 }
 
-button{
+button {
     margin: 0 1em
 }
 
-li{
+li {
     font-size: 2em;
 }
 </style>
