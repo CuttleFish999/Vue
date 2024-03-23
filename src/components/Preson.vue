955fb35b781b107d2a018949fc0618d6bebc6c7e
CuttleFish <b9804320975@gmail.com>
2024-03-23 14:50:17 +0800

    typeScript

--- src/components/Preson.vue
+++ src/components/Preson.vue
@@ -1,25 +1,15 @@
 <template>
     <div class="person">
-       <h1 ref="title2">台北</h1>
-       <h1 id="title2">桃園</h1>
-
-       <button @click="showLog">輸出</button>
+        ???
     </div>
 </template>
 
 
 
 <script setup lang="ts" name="Person">
-import { ref, watch , watchEffect } from 'vue'
-
-let title2 = ref()
-
-
-function showLog(){
-    console.log(title2.value)
-}
-
-
+    import {type  PersonInter} from '@/types'
+    
+    let person:PersonInter = {id:'123' , name:'222',age:20}
 </script>
 
 
