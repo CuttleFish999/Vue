f6351386c39867ad51b7c7bcf1d9b208f1a1b98e
CuttleFish <b9804320975@gmail.com>
2024-03-23 20:41:31 +0800

    axios

--- src/components/Preson.vue
+++ src/components/Preson.vue
@@ -1,19 +1,38 @@
 <template>
     <div class="person">
-        <h2>{{ a }}</h2>
-        <ul>
-            <li v-for="a in list" :key="a.id">{{ a.id }} - {{ a.name }} - {{ a.age }}</li>
-        </ul>
+        <h2>sum:{{ sum }}</h2>
+        <button @click="add">add</button>
     </div>
+    <hr>
+    <img v-for="(dog,index) in dogList" :src="dog" alt="" :key="index">
+    <button @click="getDog">oneMoreDog</button>
 </template>
 
 
 
 <script setup lang="ts" name="Person">
-    import {defineProps} from 'vue'
+    import {ref , reactive} from 'vue'
+    import axios from 'axios'
 
-    let x = defineProps(['a','list'])
-    console.log(x.a)
+    let sum = ref(0)
+    let dogList = reactive([
+        'https:\/\/images.dog.ceo\/breeds\/pembroke\/n02113023_1136.jpg',
+        'https:\/\/images.dog.ceo\/breeds\/pembroke\/n02113023_6140.jpg'
+    ])
+
+    function add(){
+        sum.value ++
+    }
+
+    async function getDog(){
+        try{
+            let result = await axios.get('https://dog.ceo/api/breed/pembroke/images/random')
+            dogList.push(result.data.message)
+        }catch(error){
+            alert(error)
+        }
+        
+    }
 </script>
 
 
@@ -36,5 +55,9 @@ button {
 
 li {
     font-size: 2em;
+}
+img{
+    height: 100px;
+    margin-right: 1em;
 }
 </style>
