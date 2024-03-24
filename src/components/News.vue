c9994318551c36e41efe1fe3868d849a762fa053
CuttleFish <b9804320975@gmail.com>
2024-03-24 13:19:46 +0800

    get分別傳參3

--- src/components/News.vue
+++ src/components/News.vue
@@ -2,7 +2,19 @@
     <div class="news">
         <ul>
             <li v-for="news in newList" :key="news.id">
-                <RouterLink :to="`/News/Detail?id=${news.id}&title=${news.title}&content=${news.content}`">{{news.title}}</RouterLink>
+                <!-- <RouterLink :to="`/News/Detail?id=${news.id}&title=${news.title}&content=${news.content}`">{{news.title}}</RouterLink> -->
+                <RouterLink 
+                    :to="{
+                    path:'/News/Detail?',
+                    query:{
+                        id:news.id,
+                        title:news.title,
+                        content:news.content
+                    }
+                }"
+                >
+                    {{ news.title }}
+                </RouterLink>
             </li>
         </ul>
     </div>
@@ -13,18 +25,16 @@
 </template>
 
 <script setup lang="ts" name="News">
-import {reactive} from 'vue'
+import { reactive } from 'vue'
 
 const newList = reactive([
-    {id:"1" , title:"abc" , content:"123"},
-    {id:"2" , title:"abcd" , content:"1233"},
-    {id:"3" , title:"abce" , content:"1234"},
-    {id:"4" , title:"abcf" , content:"1232"}
+    { id: "1", title: "abc", content: "123" },
+    { id: "2", title: "abcd", content: "1233" },
+    { id: "3", title: "abce", content: "1234" },
+    { id: "4", title: "abcf", content: "1232" }
 ])
 
 </script>
 
 
-<style>
-
-</style>
+<style></style>
