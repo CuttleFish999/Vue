41551780c2f274efaae16e816fbd882b6c5c4df9
CuttleFish <b9804320975@gmail.com>
2024-03-24 17:57:01 +0800

    pinia init

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,13 @@ import App from './App.vue'
 
 import router from './router'
 
+import {createPinia} from 'pinia'
+
 
 const app = createApp(App)
+const pinia = createPinia()
 
 app.use(router)
 
+app.use(pinia)
 app.mount('#app')
